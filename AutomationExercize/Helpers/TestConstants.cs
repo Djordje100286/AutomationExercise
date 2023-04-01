dc778c821cafd1a25eb9f80c0a0a71934332e220
valentinatad <djordje.tadic86@gmail.com>
2023-04-01 12:37:40 +0200

    Initial Commit

---
+++ AutomationExercize/Helpers/TestConstants.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationExercize.Helpers
+{
+    public static class TestConstants
+    {
+        //existing user data
+        public const string Username = "djordje.tadic86@gmail.com";
+        public const string Password = "Sof14jef.";
+
+        //new user data
+        public const string FirstName = "Djordje";
+        public const string LastName = "Tadic";
+        public const string Address = "Cara Lazara 9";
+        public const string Country = "Canada";
+        public const string State = "Ontario";
+        public const string City = "Montreal";
+        public const string ZipCode = "10010";
+        public const string Phone = "0800100100";
+    }
+}
